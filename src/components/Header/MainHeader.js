2f3d977d1b81014c5419d6bfb1ea51df0e8dcfe3
Biszku <dbichu@onet.pl>
2023-03-28 08:25:04 +0200

    added low quality video

--- src/components/Header/MainHeader.js
+++ src/components/Header/MainHeader.js
@@ -1,6 +1,6 @@
 import HeaderNav from "./Nav";
 import HeaderText from "./HeaderText";
-import MainVideo from "../../assets/video/coverr-toasting-with-champagne-4587-original.mp4";
+import MainVideo from "../../assets/video/coverr-toasting-with-champagne-4587-1080p.mp4";
 
 function MainHeader() {
   return (
