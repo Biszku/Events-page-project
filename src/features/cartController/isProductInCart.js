3bc2e9144e06490b3cb14b44a4e85b9dd575a32c
Biszku <dbichu@onet.pl>
2024-02-15 11:50:42 +0100

    added condition

--- src/features/cartController/isProductInCart.js
+++ src/features/cartController/isProductInCart.js
@@ -1,4 +1,5 @@
 const isEventInCart = (cartArr, event) => {
+  if (!event?.stats.lowest_price) return true;
   const isEventInCart = cartArr.find((e) => e?.id === event?.id);
   if (isEventInCart) return true;
   return false;
