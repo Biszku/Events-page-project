568b9ba8fa5f84c08cf6176254e52bc5c64ddc5d
Biszku <dbichu@onet.pl>
2024-02-16 21:08:21 +0100

    implementing react hook form

--- src/components/popUp/formDeliverySection/formDeliveryComponent.jsx
+++ src/components/popUp/formDeliverySection/formDeliveryComponent.jsx
@@ -5,13 +5,10 @@ const FormDeliveryComponent = ({ page, visibility, setPage }) => {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm();
 
   const onSubmit = (data) => console.log(data);
-
-  console.log(watch("example"));
 
   return (
     <div
@@ -22,105 +19,114 @@ const FormDeliveryComponent = ({ page, visibility, setPage }) => {
         page === 2 && visibility ? "pageVisible" : ""
       }`}
     >
-      <div className="popup-cart__content-2_form">
-        <div className="popup-cart__content-2_form-item">
-          <p>First Name </p>
-          <input
-            className="popup-cart__content-2_form-item-input"
-            placeholder="John/Jane"
-            type="text"
-          />
-          <p>Last Name </p>
-          <input
-            className="popup-cart__content-2_form-item-input"
-            placeholder="Doe"
-            type="text"
-          />
-        </div>
-        <div className="popup-cart__content-2_form-item">
-          <p>Email </p>
-          <input
-            className="popup-cart__content-2_form-item-input"
-            placeholder="anonymou@gmail.com"
-            type="email"
-          />
-          <p>Phone Number </p>
-          <input
-            className="popup-cart__content-2_form-item-input"
-            placeholder="+12 345 678 912"
-            type="tel"
-          />
-        </div>
-        <div className="popup-cart__content-2_form-item">
-          <p>Address </p>
-          <textarea
-            placeholder="Enter address"
-            className="popup-cart__content-2_form-item-input popup-cart__content-2_form-textarea"
-            rows="2"
-            cols="50"
-          />
-          <p>Postal code </p>
-          <input
-            className="popup-cart__content-2_form-item-input"
-            placeholder="Enter postal code"
-            type="text"
-          />
-        </div>
-      </div>
-      <div className="popup-cart__content-2_delivery">
-        <p className="popup-cart__content-2_delivery-header">Delivery Type</p>
-        <div className="popup-cart__content-2_delivery_form-item">
-          <input
-            style={{
-              display: "none",
-            }}
-            className="popup-cart__content-2_form-item-input popup-cart__content-2_delivery_form-item-radio"
-            type="radio"
-            name="delivery"
-            id="Radio1"
-          />
-          <label htmlFor="Radio1">
-            <div
-              className="popup-cart__content-2_delivery_form-item-radio-style"
-              value="Courier Delivery (online payment)"
-            ></div>
-          </label>
-          <ImTruck className="popup-cart__content-2_delivery_form-item-icon" />
-          <span className="popup-cart__content-2_delivery_form-item-text">
-            {`Courier Delivery (online payment)`}
-          </span>
-        </div>
-        <div className="popup-cart__content-2_delivery_form-item">
-          <input
-            style={{
-              display: "none",
-            }}
-            className="popup-cart__content-2_form-item-input popup-cart__content-2_delivery_form-item-radio"
-            type="radio"
-            name="delivery"
-            id="Radio2"
-          />
-          <label htmlFor="Radio2">
-            <div
-              className="popup-cart__content-2_delivery_form-item-radio-style"
-              value="Courier Delivery (cash on delivery)"
-            ></div>
-          </label>
-          <ImTruck className="popup-cart__content-2_delivery_form-item-icon" />
-          <span className="popup-cart__content-2_delivery_form-item-text">
-            {`Courier Delivery (cash on delivery)`}
-          </span>
-        </div>
-      </div>
-      <button
-      
-        onClick={() => {
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
           setPage((prev) => prev + 1);
         }}
-        className="popup-cart__content-2-btn"
       >
-        Finish Transaction
-      </button>
+        <div className="popup-cart__content-2_form">
+          <div className="popup-cart__content-2_form-item">
+            <p>First Name </p>
+            <input
+              className="popup-cart__content-2_form-item-input"
+              placeholder="John/Jane"
+              type="text"
+              {...register("firstName", { required: true })}
+            />
+            <p>Last Name </p>
+            <input
+              className="popup-cart__content-2_form-item-input"
+              placeholder="Doe"
+              type="text"
+              {...register("lastName", { required: true })}
+            />
+          </div>
+          <div className="popup-cart__content-2_form-item">
+            <p>Email </p>
+            <input
+              className="popup-cart__content-2_form-item-input"
+              placeholder="anonymou@gmail.com"
+              type="email"
+              {...register("emailAddress", { required: true })}
+            />
+            <p>Phone Number </p>
+            <input
+              className="popup-cart__content-2_form-item-input"
+              placeholder="+12 345 678 912"
+              type="tel"
+              {...register("phoneNumber", { required: true })}
+            />
+          </div>
+          <div className="popup-cart__content-2_form-item">
+            <p>Address</p>
+            <textarea
+              placeholder="Enter address"
+              className="popup-cart__content-2_form-item-input popup-cart__content-2_form-textarea"
+              rows="2"
+              cols="50"
+              {...register("deliveryAddress", { required: true })}
+            />
+            <p>Postal code </p>
+            <input
+              className="popup-cart__content-2_form-item-input"
+              placeholder="Enter postal code"
+              type="text"
+              {...register("postalCode", { required: true })}
+            />
+          </div>
+        </div>
+        <div className="popup-cart__content-2_delivery">
+          <p className="popup-cart__content-2_delivery-header">Delivery Type</p>
+          <div className="popup-cart__content-2_delivery_form-item">
+            <input
+              style={{
+                display: "none",
+              }}
+              className="popup-cart__content-2_form-item-input popup-cart__content-2_delivery_form-item-radio"
+              type="radio"
+              name="delivery"
+              id="Radio1"
+            />
+            <label htmlFor="Radio1">
+              <div
+                className="popup-cart__content-2_delivery_form-item-radio-style"
+                value="Courier Delivery (online payment)"
+              ></div>
+            </label>
+            <ImTruck className="popup-cart__content-2_delivery_form-item-icon" />
+            <span className="popup-cart__content-2_delivery_form-item-text">
+              {`Courier Delivery (online payment)`}
+            </span>
+          </div>
+          <div className="popup-cart__content-2_delivery_form-item">
+            <input
+              style={{
+                display: "none",
+              }}
+              className="popup-cart__content-2_form-item-input popup-cart__content-2_delivery_form-item-radio"
+              type="radio"
+              name="delivery"
+              id="Radio2"
+            />
+            <label htmlFor="Radio2">
+              <div
+                className="popup-cart__content-2_delivery_form-item-radio-style"
+                value="Courier Delivery (cash on delivery)"
+              ></div>
+            </label>
+            <ImTruck className="popup-cart__content-2_delivery_form-item-icon" />
+            <span className="popup-cart__content-2_delivery_form-item-text">
+              {`Courier Delivery (cash on delivery)`}
+            </span>
+          </div>
+        </div>
+        <input
+          type="submit"
+          value="Finish Transaction"
+          className="popup-cart__content-2-btn"
+        />
+      </form>
     </div>
   );
 };
